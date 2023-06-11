af27dc05f0663c0f007da7d3b4c3e86a39ece6ec
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2023-06-12 02:26:13 +0530

    How To Add Search Bar In Your Mobile Application 🔎📲👨‍🎓
    How To Design Search For Your Mobile App 🔎📲💥

---
+++ youtube_short/lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Search Bar',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<Map<String, dynamic>> _allUsers = [
+    {"id": 1, "name": "Lapz", "age": 25},
+    {"id": 2, "name": "CodeShow", "age": 30},
+    {"id": 3, "name": "Brain", "age": 50},
+    {"id": 4, "name": "Jack", "age": 35},
+    {"id": 5, "name": "Azam", "age": 28},
+    {"id": 6, "name": "Sarth", "age": 45},
+    {"id": 7, "name": "Zonal", "age": 30},
+    {"id": 8, "name": "Ishu", "age": 15},
+    {"id": 9, "name": "Depz", "age": 55},
+    {"id": 10, "name": "Sewz", "age": 10},
+  ];
+
+  List<Map<String, dynamic>> _foundUsers = [];
+  @override
+  initState() {
+    _foundUsers = _allUsers;
+    super.initState();
+  }
+
+  void _runFilter(String enteredKeyword) {
+    List<Map<String, dynamic>> results = [];
+    if (enteredKeyword.isEmpty) {
+      results = _allUsers;
+    } else {
+      results = _allUsers
+          .where((user) =>
+              user["name"].toLowerCase().contains(enteredKeyword.toLowerCase()))
+          .toList();
+    }
+
+    setState(() {
+      _foundUsers = results;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Search Bar'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              onChanged: (value) => _runFilter(value),
+              decoration: const InputDecoration(
+                  labelText: 'Search', suffixIcon: Icon(Icons.search)),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: _foundUsers.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: _foundUsers.length,
+                      itemBuilder: (context, index) => Card(
+                        key: ValueKey(_foundUsers[index]["id"]),
+                        color: Color.fromARGB(255, 255, 221, 253),
+                        elevation: 4,
+                        margin: const EdgeInsets.symmetric(vertical: 10),
+                        child: ListTile(
+                          leading: Text(
+                            _foundUsers[index]["id"].toString(),
+                            style: const TextStyle(fontSize: 24),
+                          ),
+                          title: Text(_foundUsers[index]['name']),
+                          subtitle: Text(
+                              '${_foundUsers[index]["age"].toString()} years old'),
+                        ),
+                      ),
+                    )
+                  : const Text(
+                      'No results found',
+                      style: TextStyle(fontSize: 24),
+                    ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
